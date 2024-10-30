fd04a23c942a30f54830cd8f2a2fc01fcd94d9b7
Denner Freitas <dennerwillian01@gmail.com>
2024-10-29 21:19:11 -0300

    feat: built the registration options screen and adjusted the screens navigation method

--- lib/view/cadastro_page.dart
+++ lib/view/cadastro_page.dart
@@ -1,18 +1,115 @@
 import 'package:flutter/material.dart';
 
-class CadastroPage extends StatefulWidget {
+class CadastroPage extends StatelessWidget {
   const CadastroPage({super.key});
 
   @override
-  State<CadastroPage> createState() => _CadastroPageState();
-}
-
-class _CadastroPageState extends State<CadastroPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Cadastro de Alimentos Page"),
+      backgroundColor: const Color(0xFFCCFFD1), // Cor de fundo verde claro
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Stack(
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          size: 36, // Aumenta o tamanho do ícone
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          // Ação de voltar
+                        },
+                      ),
+                      Positioned(
+                        left:
+                            2, // Desloca levemente para a direita para simular "bold"
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.arrow_back,
+                            size: 36,
+                            color: Colors.black,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "O que você deseja cadastrar?",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 295,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Ação para cadastrar novo alimento
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        const Color(0xFFF46472), // Cor de fundo do botão
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    "Novo Alimento",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: 295,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Ação para cadastrar novo cardápio
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        const Color(0xFFF46472), // Cor de fundo do botão
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    "Novo Cardápio",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
